8ea75f7118f3728db6f9e0935720125a2d48a602
csslick <digitone@naver.com>
2020-01-21 23:18:46 +0900

    deploy server port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000 
 const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database('db/comments.db')
 
